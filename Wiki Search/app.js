d1b0865238b37a848f1663813f612819b81f1c50
Shreyas Bangera <s.bangera35@gmail.com>
2016-10-24 23:56:21 +0530

    Add files via upload

---
+++ Wiki Search/app.js
@@ -0,0 +1,38 @@
+    $(document).ready(function(){
+        $("#search").on('keyup',function(){
+            var searchKey=document.getElementById("search").value;
+            $("#result").html("");
+            document.getElementById("dataHeader").style.visibility='visible';
+            if(searchKey==="")
+            {
+                document.getElementById("dataHeader").style.visibility='hidden';
+            }
+            else
+            {
+                $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&search='+searchKey+'&format=json&callback=?', function(data){
+                for (var i in data) {
+                    if(data[1][i] === undefined)
+                    {
+                        $("#result").html("");
+                        $('#result').append('<div id="articles" class="well"><h3 class="text-center">No Results.....</h3></div>');
+                        break;
+                    }
+                    else
+                    {
+                        $("#result").animate({'margin-top': '-8px'}, 500, "linear");
+                        $('#result').append('<div id="articles" class="well"><a class="title" id="alink" href="'+data[3][i]+'"target="_blank"><h3 class="text-center">' + data[1][i] + '</h3></a><p class="cont">' + data[2][i] + '</p></div>');
+
+                    }
+                }
+                });
+            }
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $(".btn").click(function(){
+       window.open("https://en.wikipedia.org/wiki/Special:Random", "_blank");
+    });
+
+
+});
